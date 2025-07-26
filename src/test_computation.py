e49e4a0de6426d31721fb850a3db58b446022a92
kittipattan <putterkittipat@gmail.com>
2025-07-26 23:32:45 +0700

    Add homomac computation time evaluation

--- src/test_computation.py
+++ src/test_computation.py
@@ -1,7 +1,23 @@
-from typing import Callable, Any
+from typing import Callable, Any, List, Tuple
 from charm.toolbox.pairinggroup import PairingGroup, G1, G2, GT, ZR
 from utils.misc import measure_computation_time
+from utils.mac import HomomorphicMAC
 import hmac, hashlib, os, random, time
+
+def test_homomac(mac: HomomorphicMAC, group: PairingGroup, tag_count: int) -> Tuple[List, List]:
+    messages = []
+    tags = []
+    for _ in range(tag_count):
+        m = group.hash("A"*1000, ZR)    # Transform message into integer
+        messages.append(m)
+        tags.append(mac.sign(m))
+
+    return tags, messages
+
+def test_homomac_time(mac: HomomorphicMAC, tags: List, hashes: List):
+    agg_tag = mac.aggregate_tags(tags)
+    hs_sum = sum(hashes)
+    mac.verify(hs_sum, agg_tag)
 
 if __name__ == "__main__":
     group = PairingGroup('SS512')   # supersingular elliptic curve / Type-A / symmetric
@@ -12,11 +28,20 @@ if __name__ == "__main__":
 
     key = os.urandom(32)
     message = random.randbytes(100)
-    
-    print('a: ', a)
-    print('b: ', b)
-    print('g0: ', g0)
-    print('g1: ', g1)
+
+    mac = HomomorphicMAC(group, group.random(ZR))
+    print("MAC:")
+    for count in [1,2,3,4,5,6,7,8,9,10]:
+        tags, hashes = test_homomac(mac, group, count)
+        print(f"{count} tags", end="\t")
+        measure_computation_time(test_homomac_time, mac, tags, hashes, iterations=5000)
+
+    # print(f'Verification of aggregated tag is {"successful" if valid else "unsuccessful"}')
+
+    # print('a: ', a)
+    # print('b: ', b)
+    # print('g0: ', g0)
+    # print('g1: ', g1)
 
     print("Modular Exponentiation: ")
     measure_computation_time(lambda: a**b, iterations=10000)
